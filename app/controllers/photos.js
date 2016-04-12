28ffc6b4cca7c61f76ba1db7fa2d28d810e52013
truman <zeiontic@gmail.com>
2016-04-12 17:39:57 -0400

    links and instructions

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    queryParams: ['album_id'],
-    album_id: null
+    queryParams: ['album_id', 'album_title'],
+    album_id: null,
+    album_title: null
 });
--- app/routes/photos.js
+++ app/routes/photos.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
+    console.log(params)
         return Ember.$.getJSON("http://jsonplaceholder.typicode.com/photos?albumId=" + params.album_id);
     }
 });
